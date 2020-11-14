1cb4388196ec361921c78c4bb628f4177796700a
oli21j457 <74430240+oli21j457@users.noreply.github.com>
2020-11-14 15:52:49 -0400

    Add files via upload

---
+++ principal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "NOMBRE COMPLETO:Olivia Herrera Tapia" << endl;
+	cout << "C.U:35-4926" << endl;
+	cout << "C.I:12515779 Ch" << endl;
+	cout << "CARRERA:Ingenieria de sistemas" << endl;
+	cout << "EDAD: 19 aa" << endl;
+	system("pause");
+	return 0;
+}
